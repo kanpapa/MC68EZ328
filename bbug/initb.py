53e6012ffda7cd27f3cf55ff1fa36c4ebe3343e7
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2021-04-23 08:46:53 +0900

    Added initb.py

--- bbug/initb.py
+++ bbug/initb.py
@@ -38,11 +38,6 @@ def ser_send(ser, brec):
 
     return 0
 
-# Get dump address and count
-dumpadrs = int(sys.argv[1],16)
-dumpcnt  = int(sys.argv[2],16)
-print(format(dumpadrs, '08X'), format(dumpcnt, '04X'))
-
 # Open Serial port
 ser = serial.Serial('/dev/ttyUSB0',9600)
 
@@ -51,32 +46,25 @@ ser.write(13)
 line = ser.read()
 print(line)
 
-# main loop
-for i in range(dumpcnt):
-    # display memory address
-    m = i % 16
-    if m == 0:
-        print("\n", end='')
-        print(format(dumpadrs, '08X'), end='')
-        print(" ", end='')
-
-    # load code
-    ser_send(ser, "\nFFFFFFAA082C7C")
-    ser_send(ser, format(dumpadrs, '08X'))
-    ser_send(ser, "4E71")
-    ser_send(ser, "\nFFFFFFAA00")
-    ser_send(ser, "\nFFFFFFAA082A7CFFFFF9074E71")
-    ser_send(ser, "\nFFFFFFAA00")
-    ser_send(ser, "\nFFFFFFAA081A964E714E714E71")
-    ser_send(ser, "\nFFFFFFAA00")
-
-    # display feedback data
-    val = ser.read()
-    print(format(val[0], '02X'), end='')
-    print(" ", end='')
-
-    # next address
-    dumpadrs = dumpadrs + 1
+# init.b code
+ser_send(ser, "\nFFFFF1180130")   # emucs init 4-8 not needed without emuchip
+ser_send(ser, "\nFFFFF0000110")   # SCR init 3-2
+ser_send(ser, "\nFFFFFB0B0100")   # Disable WD 13-5
+ser_send(ser, "\nFFFFF42B0103")   # enable clko 7-12
+ser_send(ser, "\nFFFFF40B0100")   # set as dedicated chip selects 7-4
+ser_send(ser, "\nFFFFF4230100")   # enable *DWE 7-11
+ser_send(ser, "\nFFFFFD0D0108")   # disable hardmap 15-6 $0000 is $fffc
+ser_send(ser, "\nFFFFFD0F0107")   # clear level 7 interrupt 15-8
+ser_send(ser, "\nFFFFF100028000") # CSA at 10000000
+ser_send(ser, "\nFFFFF1100201ED") # 8 x 1M - KH
+ser_send(ser, "\nFFFFF102028000") # CSB
+ser_send(ser, "\nFFFFF112020190") # disable eth + DPRAM - KH
+ser_send(ser, "\nFFFFFC00028F00") # DRAM Config at $0 14-6
+ser_send(ser, "\nFFFFFC02028667") # DRAM Control
+ser_send(ser, "\nFFFFF106020000") # CSD init at 0 -- RAS0 4M-6M, RAS1 6M-8M 4-4
+ser_send(ser, "\nFFFFF11602068D") # enable DRAM cs - KH
+ser_send(ser, "\nFFFFF3000140")   # IVR
+ser_send(ser, "\nFFFFF30404007FFFFF") # IMR
 
 print("\n\nend.")
 
